75e0242b752ac9900cb7892507e2a301f2b56795
ytsujiii <tsujimon850734@gmail.com>
2022-03-31 14:54:02 +0900

    apply formatter

--- 9cc.c
+++ 9cc.c
@@ -1,22 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
 #include <ctype.h>
 #include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef enum {
-    TK_RESERVED,
-    TK_NUM,
-    TK_EOF,
+  TK_RESERVED,
+  TK_NUM,
+  TK_EOF,
 } TokenKind;
 
 typedef struct Token Token;
 
 struct Token {
-    TokenKind kind;
-    Token *next;
-    int val;
-    char *str;
+  TokenKind kind;
+  Token *next;
+  int val;
+  char *str;
 };
 
 Token *token;
@@ -36,93 +36,92 @@ void error_at(char *loc, char *fmt, ...) {
 }
 
 bool consume(char op) {
-    if (token->kind != TK_RESERVED || token->str[0] != op)
-        return false;
-    token = token->next;
-    return true;
+  if (token->kind != TK_RESERVED || token->str[0] != op)
+    return false;
+  token = token->next;
+  return true;
 }
 
 void expect(char op) {
-    if (token->kind != TK_RESERVED || token->str[0] != op)
-        error_at(token->str, "'%c'ではありません", op);
-    token = token->next;
+  if (token->kind != TK_RESERVED || token->str[0] != op)
+    error_at(token->str, "'%c'ではありません", op);
+  token = token->next;
 }
 
 int expect_number() {
-    if (token->kind != TK_NUM)
-        error_at(token->str, "数ではありません");
-    int val = token->val;
-    token = token->next;
-    return val;
+  if (token->kind != TK_NUM)
+    error_at(token->str, "数ではありません");
+  int val = token->val;
+  token = token->next;
+  return val;
 }
 
 bool at_eof() {
-    return token->kind == TK_EOF;
+  return token->kind == TK_EOF;
 }
 
 Token *new_token(TokenKind kind, Token *cur, char *str) {
-    Token *tok = calloc(1, sizeof(Token));
-    tok->kind = kind;
-    tok->str = str;
-    cur->next = tok;
-    return tok;
+  Token *tok = calloc(1, sizeof(Token));
+  tok->kind = kind;
+  tok->str = str;
+  cur->next = tok;
+  return tok;
 }
 
 Token *tokenize(char *p) {
-    Token head;
-    head.next = NULL;
-    Token *cur = &head;
+  Token head;
+  head.next = NULL;
+  Token *cur = &head;
 
-    while (*p) {
-        if (isspace(*p)) {
-            p++;
-            continue;
-        }
-
-        if (*p == '+' || *p == '-') {
-            cur = new_token(TK_RESERVED, cur, p++);
-            continue;
-        }
-
-        if (isdigit(*p)) {
-            cur = new_token(TK_NUM, cur, p);
-            cur->val = strtol(p, &p, 10);
-            continue;
-        }
-
-        error_at(token->str, "トークナイズできません");
+  while (*p) {
+    if (isspace(*p)) {
+      p++;
+      continue;
     }
 
-    new_token(TK_EOF, cur, p);
-    return head.next;
+    if (*p == '+' || *p == '-') {
+      cur = new_token(TK_RESERVED, cur, p++);
+      continue;
+    }
+
+    if (isdigit(*p)) {
+      cur = new_token(TK_NUM, cur, p);
+      cur->val = strtol(p, &p, 10);
+      continue;
+    }
+
+    error_at(token->str, "トークナイズできません");
+  }
+
+  new_token(TK_EOF, cur, p);
+  return head.next;
 }
 
 int main(int argc, char **argv) {
-    if (argc != 2) {
-        fprintf(stderr, "引数の個数が正しくありません\n");
-        return 1;
+  if (argc != 2) {
+    fprintf(stderr, "引数の個数が正しくありません\n");
+    return 1;
+  }
+
+  user_input = argv[1];
+  token = tokenize(argv[1]);
+
+  printf(".intel_syntax noprefix\n");
+  printf(".globl main\n");
+  printf("main:\n");
+
+  printf("  mov rax, %d\n", expect_number());
+
+  while (!at_eof()) {
+    if (consume('+')) {
+      printf("  add rax, %d\n", expect_number());
+      continue;
     }
 
-    user_input = argv[1];
-    token = tokenize(argv[1]);
+    expect('-');
+    printf("  sub rax, %d\n", expect_number());
+  }
 
-    printf(".intel_syntax noprefix\n");
-    printf(".globl main\n");
-    printf("main:\n");
-
-    printf("  mov rax, %d\n", expect_number());
-
-    while (!at_eof()) {
-        if (consume('+')) {
-            printf("  add rax, %d\n", expect_number());
-            continue;
-        }
-
-        expect('-');
-        printf("  sub rax, %d\n", expect_number());
-    }
-
-    printf("  ret\n");
-    return 0;
+  printf("  ret\n");
+  return 0;
 }
-
