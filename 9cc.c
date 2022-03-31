06c52125dcf03fa04fc35ab3574a32a149fdf344
ytsujiii <tsujimon850734@gmail.com>
2022-03-31 15:13:24 +0900

    token parser

--- 9cc.c
+++ 9cc.c
@@ -10,7 +10,16 @@ typedef enum {
   TK_EOF,
 } TokenKind;
 
+typedef enum {
+  ND_ADD,
+  ND_SUB,
+  ND_MUL,
+  ND_DIV,
+  ND_NUM,
+} NodeKind;
+
 typedef struct Token Token;
+typedef struct Node Node;
 
 struct Token {
   TokenKind kind;
@@ -19,8 +28,66 @@ struct Token {
   char *str;
 };
 
+struct Node {
+  NodeKind kind;
+  Node *lhs;
+  Node *rhs;
+  int val;
+}
+
 Token *token;
 char *user_input;
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node *new_node_num(int val) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = ND_NUM;
+  node->val = val;
+  return node;
+}
+
+Node *expr() {
+  Node *node = mul();
+
+  for (;;) {
+    if (consume('+'))
+      node = new_node(ND_ADD, node, mul());
+    else if (consume('-'))
+      node = new_node(ND_SUM, node, mul());
+    else
+      return node;
+  }
+}
+
+Node *mul() {
+  Node *node = primary();
+
+  for (;;) {
+    if (consume('*'))
+      node = new_node(ND_MUL, node, primary());
+    else if (consume('/'))
+      node = new_node(ND_DIV, node, primary());
+    else
+      return node;
+  }
+}
+
+Node *primary() {
+  if (consume('(')) {
+    Node *node = expr();
+    expect(')');
+    return node;
+  }
+
+  return new_node_num(expect_number());
+}
 
 void error_at(char *loc, char *fmt, ...) {
   va_list ap;
