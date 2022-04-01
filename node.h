5eeb529827c9fed72399b1e042c88839f94c2e81
ytsujiii <tsujimon850734@gmail.com>
2022-04-01 16:11:08 +0900

    separate Node features from the main script

---
+++ node.h
@@ -0,0 +1,31 @@
+#ifndef _node_h_
+#define _node_h_
+
+typedef enum {
+  ND_ADD,
+  ND_SUB,
+  ND_MUL,
+  ND_DIV,
+  ND_NUM,
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node {
+  NodeKind kind;
+  Node *lhs;
+  Node *rhs;
+  int val;
+};
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs);
+Node *new_node_num(int val);
+
+Node *expr();
+Node *mul();
+Node *primary();
+Node *unary();
+
+void gen();
+
+#endif
