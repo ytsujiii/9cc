3c599a716d75ed0ff45b87cb16d7ed8bb970641e
ytsujiii <tsujimon850734@gmail.com>
2022-04-01 14:58:45 +0900

    expect new characters as a reserved token

--- 9cc.c
+++ 9cc.c
@@ -146,7 +146,8 @@ Token *tokenize(char *p) {
       continue;
     }
 
-    if (*p == '+' || *p == '-') {
+    if (*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' ||
+        *p == ')') {
       cur = new_token(TK_RESERVED, cur, p++);
       continue;
     }
